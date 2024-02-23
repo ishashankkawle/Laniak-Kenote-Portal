07035549656b79a1c3af32546d18d2c7d292bf63
Shashank Kawle <skawle631@gmail.com>
2024-02-23 18:26:49 +0530

    Minor update

--- app/page.js
+++ app/page.js
@@ -36,7 +36,7 @@ export default function Home() {
             <blockquote className="blockquote">
               Learn and evolve through the super cluster of tech-science
             </blockquote>
-            Laniak is knowledge sharing hub here you can learn <br /> through the experices shared by community via docs to get optimum <br /> solution for your problem
+            Keynote is knowledge sharing hub here you can learn <br /> through the experices shared by community via docs to get optimum <br /> solution for your problem
           </div>
         </div>
       </div>
@@ -45,7 +45,7 @@ export default function Home() {
         <div className={`${styles.introHeading} mt-5 mb-5`}>
           <div>
             <h2>
-              Why Laniak?
+              Why Keynote?
             </h2>
           </div>
         </div>
@@ -56,7 +56,7 @@ export default function Home() {
             <span className={`${styles.featureNum} col-2`}>01</span>
             <div className={`${styles.featureText} col-6`}>
               <h4 className={`${styles.featureHeading}`}><b>Easy to Understand.</b></h4>
-              <p>Laniak docs are created in simplest terms that are easy to quickly understand and absorb. Sharing their own experiene through notes laniak will connect you with developers around the world</p>
+              <p>Keynotes are created in simplest terms that are easy to quickly understand and absorb. Sharing their own experiene through notes laniak will connect you with developers around the world</p>
             </div>
             <Image src={feature1} className={`${styles.featureImg} float-end`} alt='feature 1'/>
           </div>
@@ -66,7 +66,7 @@ export default function Home() {
             <Image src={feature2} className={`${styles.featureImg}`} alt='feature 2' />
             <div className={`${styles.featureText} col-6 ms-auto`}>
               <h4 className={`${styles.featureHeading}`}><b>More Productive.</b></h4>
-              <p>Laniak opens doors to wide range of developer technologies. Developers around thee world can help through laniak for you to be more productive and make your own legacy application</p>
+              <p>Keynote opens doors to wide range of developer technologies. Developers around thee world can help through laniak for you to be more productive and make your own legacy application</p>
             </div>
           </div>
 
@@ -74,7 +74,7 @@ export default function Home() {
             <span className={`${styles.featureNum} col-2`}>03</span>
             <div className={`${styles.featureText} col-6`}>
               <h4 className={`${styles.featureHeading}`}><b>Backed by huge community.</b></h4>
-              <p>Laniak thanks to all the developers around the world for putting their efforts in sharing their problems and solving them throuh laniak. This laniak is created for delopers by developers itself</p>
+              <p>Keynote thanks to all the developers around the world for putting their efforts in sharing their problems and solving them throuh laniak. This laniak is created for delopers by developers itself</p>
             </div>
             <Image src={feature3} className={`${styles.featureImg} float-end`} alt='feature 3' />
           </div>
