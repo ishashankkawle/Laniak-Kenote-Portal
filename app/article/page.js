c9ee013e60940e49f6433fe1dcecc6c10479eec6
Shashank Kawle <skawle631@gmail.com>
2024-04-10 17:39:25 +0530

    Article footer update

--- app/article/page.js
+++ app/article/page.js
@@ -5,7 +5,7 @@ import { useEffect, useState } from 'react';
 import styles from './page.module.css'
 import { useRouter, useSearchParams } from 'next/navigation';
 import { httpGet } from '../_services/httpHandler';
-import { ChevronDown } from 'react-feather';
+import { ChevronDown, Heart, Link } from 'react-feather';
 import Filetree from '@/components/filetree/filetree'
 import Display from '@/components/display/display'
 import { getFullScreenLoader } from '@/components/preloader/preloader';
@@ -48,14 +48,33 @@ export default function article() {
     else {
         return (
             <div className={`${styles.articleDisplay} ms-1 px-5 py-3`}>
-                {/* <div className={`${styles.articleDisplayTitle}`}>
-                    {displayValues.title}
-                </div>
-                <div className={`${styles.articleDisplayAuthor}`}>
-                    <span>{displayValues.author}</span>&nbsp;&nbsp;&nbsp;&nbsp;<span>{displayValues.date}</span>
-                </div> */}
                 <div className={`${styles.articleDisplayFile} mt-4`} dangerouslySetInnerHTML={displayValues.content}>
-
+                </div>
+                <div className={`${styles.articleFooter} row d-flex justify-content-between py-5`}>
+                    <div className="col-sm-12 col-md-10 col-xl-10">
+                        <div className="p-2">
+                            <div className={`${styles.articleFooterAuthorHeader}`}>
+                                - Published by
+                            </div>
+                            <div className={`${styles.articleFooterAuthor}`}>
+                                {displayValues.author}
+                            </div>
+                            <div className={`${styles.articleFooterDate}`}>
+                                {displayValues.date}
+                            </div>
+                        </div>
+                    </div>
+                    <div className={`${styles.articleFooterAction} p-2 col-sm-12 col-md-2 col-xl-2`}>
+                        <span className={`${styles.articleFooterActionButton} mx-1`}>
+                            <Heart size={20} color='red' fill='red' />
+                        </span>
+                        <span className={`${styles.articleFooterActionButton} mx-1`}>
+                            <Link size={20} color='orange'/>
+                        </span>
+                        {/* <div className={`${styles.articleFooterActionButton}`}>
+                            <Heart size={20} color='red' fill='red' />
+                        </div> */}
+                    </div>
                 </div>
             </div>
         )
