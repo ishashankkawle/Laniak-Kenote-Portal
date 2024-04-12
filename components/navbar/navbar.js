d444976697312fb62b2edc99052c2d4c3499626a
Shashank Kawle <skawle631@gmail.com>
2024-04-12 15:56:56 +0530

    Navbar responsive handling

--- components/navbar/navbar.js
+++ components/navbar/navbar.js
@@ -3,7 +3,7 @@
 
 import { useRouter } from 'next/navigation';
 import styles from './navbar.module.css'
-import { HelpCircle, Phone } from 'react-feather'
+import { FileText, Grid, HelpCircle, Phone } from 'react-feather'
 
 
 export default function Navbar()
@@ -18,8 +18,10 @@ export default function Navbar()
         <nav className={`${styles.navBlock} navbar justify-content-between`}>
           <a className={`${styles.navBrand} navbar-brand`} onClick={() => handleClick("/")}>Keynote</a>
           <div className="ms-auto d-flex">
-            <a className={`${styles.navTab} nav-item nav-link nav-btn`} onClick={() => handleClick("/catalog")}>Catalog</a>
-            <a className={`${styles.navTab} nav-item nav-link nav-btn`} onClick={() => handleClick("/gallary")}>Articles</a>
+            <a className={`${styles.navTab} ${styles.navTabText} nav-item nav-link nav-btn`} onClick={() => handleClick("/catalog")}>Catalog</a>
+            <a className={`${styles.navTab} ${styles.navTabText} nav-item nav-link nav-btn`} onClick={() => handleClick("/gallary")}>Articles</a>
+            <a className={`${styles.navTab} ${styles.navTabIcon} nav-item nav-link nav-btn`} onClick={() => handleClick("/catalog")}><Grid size={16}/></a>
+            <a className={`${styles.navTab} ${styles.navTabIcon} nav-item nav-link nav-btn`} onClick={() => handleClick("/gallary")}><FileText size={16}/></a>
             <a className={`${styles.navTab} nav-item nav-link nav-btn`}><Phone size={16} /></a>
           </div>
         </nav>
