e1f1e363f0693f1898b9ab5a889cad04a5c0af70
Shashank Kawle <skawle631@gmail.com>
2025-01-01 01:04:47 +0530

    File Tree ordering & CSS update

--- components/filetree/filetree.js
+++ components/filetree/filetree.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 'use client';
 
-import { ArrowDownCircle, ArrowLeftCircle, ArrowUpCircle, ChevronDown } from 'react-feather';
+import { ArrowDownCircle, ArrowLeftCircle, ArrowUpCircle, CheckCircle, ChevronDown, Circle } from 'react-feather';
 import styles from './filetree.module.css'
 import { useState } from 'react';
 
@@ -10,7 +10,6 @@ export default function filetree({ listdata, topic, openPage }) {
     let data = listdata
     let pathArray = []
     let nested = [];
-    let rootNode = [];
 
     let toggleDisplay = (event) => {
         event.stopPropagation()
@@ -49,86 +48,86 @@ export default function filetree({ listdata, topic, openPage }) {
 
     let openBoard = (event, data) => {
         event.stopPropagation()
+        event.target.classList.add('bg-warning-subtle')
         openPage(topic + "/" + data)
     }
 
+    //-------------------------------------------------------------
+    // HELPER FUNCTIONS
+    //-------------------------------------------------------------
     const createNestedDOM = (path) => {
 
-        //console.log("path : " + path)
         let array = data.filter(child => child.path.startsWith(path + '/'))
-
-
-        //console.log("array : " + JSON.stringify(array))
         //console.log("current pathArray : " + JSON.stringify(pathArray))
 
         // Loop through array elements
         let element = array.map((item) => {
             //console.log( " Current item : " + JSON.stringify(item) )
-
-            if (!pathArray.includes(item.path)) {
+            if (!pathArray.includes(item.path)) 
+            {
                 pathArray.push(item.path)
-
-                if (item.type === "tree") {
-
+                if (item.type === "tree") 
+                {
                     //console.log("pushing UL path  : " + item.path)            
-                    return (<ul key={item.path} onClick={toggleDisplay} className={`${styles.fileList} my-2 py-2 px-0 d-none`}>
+                    return (<ul key={item.path} onClick={toggleDisplay} className={`${styles.fileList} my-2 py-2 px-1 d-none`}>
                         <div className='d-flex justify-content-between'><span>{item.name}</span> <ChevronDown className='align-self-center' size={16} /></div>
                         {createNestedDOM(item.path)}
                     </ul>);
                 }
-                else {
-
+                else 
+                {
                     //console.log("pushing LI path : " + item.path)
                     return (<li key={item.path} value={item.path} onClick={(e) => { openBoard(e, item.path) }} className={`${styles.fileListItem} d-none my-2 py-2`}>
                         {item.name}
                     </li>)
-
                 }
             }
         });
 
         return element;
     };
+
+    const objModifier = (obj) => 
+    {
+      let arrTemp = obj.path.split("/")
+      obj.level = arrTemp.length
+      return obj
+    }
+
     //-------------------------------------------------------------
-    // END HELPER FUNCTION TO TRAVERSE INSIDE
+    // END HELPER FUNCTIONS
     //-------------------------------------------------------------
 
-    const createDirTree = () => {
-        rootNode.forEach((item) => {
+    const createDirTree = (arrData) => {
+        arrData.forEach((item) => {
             let node = undefined;
-            //console.log("item from parent : " + JSON.stringify(item))
-            pathArray.push(item.path)
-            if (item.type === "tree") {
-                node = (<ul key={item.path} onClick={toggleDisplay} className={`${styles.fileList} my-2 py-2 px-1`}>
-                    <div className='d-flex justify-content-between'><span>{item.name}</span> <ChevronDown className='align-self-center' size={16} /></div>
-                    {createNestedDOM(item.path)}
-                </ul>)
-            }
-            else {
-                node = (<li key={item.path} value={item.path} onClick={(e) => { openBoard(e, item.path) }} className={`${styles.fileListItem} my-2 py-2`}>
-                    {item.name}
-                </li>)
+            if (!pathArray.includes(item.path)) 
+            {
+                pathArray.push(item.path)
+                //console.log(pathArray)
+                if (item.type === "tree") {
+                    node = (<ul key={item.path} onClick={toggleDisplay} className={`${styles.fileList} my-2 py-2 px-1`}>
+                        <div className='d-flex justify-content-between'><span>{item.name}</span> <ChevronDown className='align-self-center' size={16} /></div>
+                        {createNestedDOM(item.path)}
+                    </ul>)
+                }
+                else {
+                    node = (<li key={item.path} value={item.path} onClick={(e) => { openBoard(e, item.path) }} className={`${styles.fileListItem} my-2 py-2`}>
+                        {item.name}
+                    </li>)
+                }
             }
 
-            //console.log(node)
             nested.push(node)
-
         })
     }
 
     //console.log("inputArray from outside" + JSON.stringify(data))
-
-    if (data != undefined) {
-        for (let index = 0; index < data.length; index++) {
-            let obj = data[index]
-            let arrPath = obj.path.split("/")
-            if (arrPath.length == 1) {
-                rootNode.push(obj)
-            }
-        }
-
-        //console.log("RootNodes : " + rootNode)
-        createDirTree()
+    if (data != undefined) 
+    {
+        data = data.map(objModifier)
+        data = data.sort((a,b) => ( a.path.localeCompare(b.path) || a.level > b.level ))
+        createDirTree(data)
     }
 
 
